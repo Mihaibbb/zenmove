54a01fbd9d98483b5e82cb25f144e866235cfe79
Mihaibbb <mihaibbb10@gmail.com>
2022-11-25 17:49:42 +0200

    altceva

---
+++ client/Colors/Main.js
@@ -0,0 +1,21 @@
+const mainColors = {
+    blue: 'rgb(81,175,254)',
+    blue2: 'rgb(65,134,255)',
+    blueBackground: 'rgba(68, 144, 255, .2)',
+
+    green: 'rgb(92,229,135)',
+    green2: 'rgb(38,207,92)',
+    greenBackground: 'rgba(73, 224, 121, .2)',
+
+    yellow: 'rgb(250,202,101)',
+    yellow2: 'rgb(250,177,41)',
+    yellowBackground: 'rgba(250, 198, 92, .2)',
+
+    purple: 'rgb(167,156,254)',
+    purple2: 'rgb(132,124,255)',
+    purpleBackground: 'rgba(161, 150, 255, .2)',
+
+    backgroundColor: 'rgb(25,26,40)'
+};
+
+export default mainColors;
